bc43e50fbf024270ca3714b0ea155598b2fbc38a
kshitij katore <kshitijkatore03@gmail.com>
2026-01-01 21:19:30 +0530

    Replicate the hero section

---
+++ Week_Warmup/browser-use/components/hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div className='hero-root'>
+        <div className='title'>
+            <div className='dot'></div>
+            <div className='side-title'>[ API FOR ANYTHING ]</div>
+        </div>
+        <div className='heading'>
+            <h1>ENABLE AI TO <span>AUTOMATE</span><br /> THE WEB</h1>
+        </div>
+        <div className='sub-heading'>The <span>ECOSYSTEM</span> built around the world's most-loved browser-automation library.</div>
+    </div>
+  )
+}
